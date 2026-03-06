72c004a248baec8b00e60d24070d42db4fc52c37
pongsiri-101 <uoyouf449@gmail.com>
2026-03-06 23:37:52 +0700

    data_frontend_success

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Category } from './category/category';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Category},
+];
